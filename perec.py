cf26b86a96013781e6c7460ed49783a822ba3f8f
Becha <zied.becha@eurecom.fr>
2020-06-03 20:59:05 +0200

    Added option to save output to file

--- perec.py
+++ perec.py
@@ -15,28 +15,44 @@ def resmanager(binary):
 	resource_manager = binary.resources_manager
 	print(resource_manager)
 	
-#Display the dialogs part of the Resources if they exist
-def dialogs(binary):
+#Display the dialogs part of the Resources if they exist + option to save it to file
+def dialogs(binary,found_o,output):
 	if not binary.resources_manager.has_dialogs:
 		print("'{}' has no dialogs. Abort!".format(binary.name), file=sys.stderr)
 		sys.exit(1)
+	j=0
 	for i in binary.resources_manager.dialogs:
 		print(i)
+		if found_o:
+			f = open(output+binary.name+"_"+"dialog"+str(j)+".txt","w")
+			f.write(str(i))
+			f.close()
+		j+=1
+	output_help(found_o)			
 
-#Display the manifest part of the Resources if they exist
-def manifest(binary):
+#Display the manifest part of the Resources if they exist + option to save it to file
+def manifest(binary,found_o,output):
 	if not binary.resources_manager.has_manifest:
 		print("'{}' has no manifest. Abort!".format(binary.name), file=sys.stderr)
 		sys.exit(1)
 	print(binary.resources_manager.manifest)
+	if found_o:
+		f = open(output+binary.name+"_"+"manifest.xml","w")
+		f.write(binary.resources_manager.manifest)
+		f.close()
+	output_help(found_o)
 
-#Display the version part of the Resources if they exist
-def version(binary):
+#Display the version part of the Resources if they exist + option to save it to file
+def version(binary,found_o,output):
 	if not binary.resources_manager.has_version:
 		print("'{}' has no version. Abort!".format(binary.name), file=sys.stderr)
 		sys.exit(1)
 	print(binary.resources_manager.version)
-
+	if found_o:
+			f = open(output+binary.name+"_"+"version.txt","w")
+			f.write(str(binary.resources_manager.version))
+			f.close()
+	output_help(found_o)
 
 #Display the types available	
 def types(binary):
@@ -45,17 +61,21 @@ def types(binary):
 		etypes.append(str(i).split(".")[1])
 	return etypes
 
-#Display infos about the icons and save them	
-def icons(binary):
+#Display infos about the icons and save them + option to save it to file	
+def icons(binary,found_o,output):
 	if not binary.resources_manager.has_icons:
 		print("'{}' has no icons. Abort!".format(binary.name), file=sys.stderr)
 		sys.exit(1)
 	i=0
 	for ico in binary.resources_manager.icons:
 		print(ico)
-		print("Icon saved as " + binary.name+str(i)+".ico")
-		ico.save(binary.name+str(i)+".ico")
+		
+		if found_o:
+			print("Icon saved as " + binary.name+str(i)+".ico")
+			ico.save(output+binary.name+str(i)+".ico")
 		i+=1
+	output_help(found_o)
+
 
 #Display the langs and sublangs
 def langue(binary):
@@ -63,14 +83,14 @@ def langue(binary):
 	 print(binary.resources_manager.sublangs_available)
 
 
-def iter(binary,arg):
+def iter(binary,arg,found_o,output):
 	etypes=types(binary)
 	if arg not in etypes:
 		print(format(binary.name)+" has no "+arg+". Abort!", file=sys.stderr)
 		sys.exit(1)
 	
 	if arg =="ICON":
-		icons(binary)
+		icons(binary,found_o,output)
 
 	elif arg =="BITMAP":
 		print("BITMAP")
@@ -81,22 +101,26 @@ def iter(binary,arg):
 		#TODO
 
 	elif arg =="DIALOG":
-		dialogs(binary)
+		dialogs(binary,found_o,output)
 
 	elif arg =="VERSION":
-		version(binary)
-		
-	elif arg =="MANIFEST":
-		manifest(binary)
+		version(binary,found_o,output)
 
-def out(binary,arg):
-	pass
-	#TODO
+	elif arg =="MANIFEST":
+		manifest(binary,found_o,output)
+
+
+#Print output message
+def output_help(found_o):
+	if not found_o:
+		print("use -o path/ to save files")
+
 
 def main(argv):
-
+	found_o=False
+	output=""
 	try:
-		opts, args = getopt.getopt(argv,"i:ats:")
+		opts, args = getopt.getopt(argv,"i:o:ats:")
 	except getopt.GetoptError:
 		sys.exit(2)
 
@@ -105,6 +129,10 @@ def main(argv):
 		if opt == '-i':
 			binary = open_pe(arg)
 			path=arg
+
+		if opt == '-o':
+			found_o= True
+			output=arg+"/"
 
 		if opt == '-a':
 			resmanager(binary)
@@ -115,12 +143,8 @@ def main(argv):
 			sys.exit()	
 
 		if opt == '-s':
-			iter(binary,arg)
+			iter(binary,arg,found_o,output)
 			sys.exit()	
-
-
-
-
 
 
 if __name__ == "__main__":
